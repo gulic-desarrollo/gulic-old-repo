91179a888e2fc32cdeabbf151a7f224f62811ea8
Alberto Morales <amd77@galotecnia.com>
2002-06-28 01:43:32 +0000

    Añadidas versiones iniciales de estos ficheros

---
+++ CILA/web/matricula.php
@@ -0,0 +1,6 @@
+<?php
+$currentsection = "Preinscripción";
+include("html/0start.html");
+include("html/matricula.html");
+include("html/0end.html");
+?>
---
+++ CILA/web/preinscripcion.php
@@ -0,0 +1,6 @@
+<?php
+$currentsection = "Preinscripción";
+include("html/0start.html");
+include("html/preinscripcion.html");
+include("html/0end.html");
+?>
