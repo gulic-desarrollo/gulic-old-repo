3461cb4bea8c602e25c3a796da060ad526ccc1f0
no_author <no_author@gulic.org>
2002-09-03 07:06:29 +0000

    This commit was manufactured by cvs2svn to create tag 'v12'.

--- CILA/web/ejemplos.php
+++
@@ -1,6 +0,0 @@
-<?php
-$currentsection = "Material";
-include("html/0start.html");
-include("html/ejemplos.html");
-include("html/0end.html");
-?>
--- CILA/web/noviembre2001.php
+++
@@ -1,6 +0,0 @@
-<?php
-$currentsection = "Ediciones";
-include("html/0start.html");
-include("html/noviembre2001.html");
-include("html/0end.html");
-?>
