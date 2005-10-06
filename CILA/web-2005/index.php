a6986fc80f610700655c9f3432d5110e90b85803
Alberto Morales <amd77@galotecnia.com>
2005-10-06 23:28:21 +0000

    Añadidas propiedades doctype y chequeador xhtml. El lateral pasa a ser parrafos y listas en vez de h2/h3.

--- CILA/web-2005/index.php
+++ CILA/web-2005/index.php
@@ -2,59 +2,73 @@
 
 // DEFINICIONES DE LA WEB
 
+$HTML='<?xml version="1.0"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>';
+
 $HEAD='<head>
-  <meta content="text/html; charset=UTF-8" http-equiv="content-type">
-  <link rel="stylesheet" type="text/css" href="img/style.css"/>
+  <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
+  <link rel="stylesheet" type="text/css" href="img/style.css" />
 ';
  
 $BEGIN_BODY = '<body>
-<div id="izqda">
-	<h2>Presentación</h2>
-	<h3><a href="index.php?q=Presentacion">Portada</a></h3>
+    <div class="izqda">
+        <p>Presentación</p>
+        <ul>
+            <li><a href="index.php?q=Presentacion">Portada</a></li>
+        </ul>
 
-	<h2>Edición Matemáticas</h2>
-	<h3><a href="index.php?q=MatematicasContenido">Contenido</a></h3>
-	<h3><a href="index.php?q=MatematicasMatricula">Matrícula</a></h3>
-	<h3><a href="index.php?q=MatematicasEvaluacion">Evaluación</a></h3>
-	<h3><a href="index.php?q=MatematicasCalificaciones">Calificaciones</a></h2>
+        <p>Edición Matemáticas</p>
+        <ul>
+            <li><a href="index.php?q=MatematicasContenido">Contenido</a></li>
+            <li><a href="index.php?q=MatematicasMatricula">Matrícula</a></li>
+            <li><a href="index.php?q=MatematicasEvaluacion">Evaluación</a></li>
+            <li><a href="index.php?q=MatematicasCalificaciones">Calificaciones</a></li>
+        </ul>
 
-	<h2>Edición Sociología</h2>
-	<h3><a href="index.php?q=SociologiaContenido">Contenido</a></h3>
-	<h3><a href="index.php?q=SociologiaMatricula">Matrícula</a></h3>
-	<h3><a href="index.php?q=SociologiaEvaluacion">Evaluación</a></h3>
-	<h3><a href="index.php?q=SociologiaCalificaciones">Calificaciones</a></h2>
+        <p>Edición Sociología</p>
+        <ul>
+            <li><a href="index.php?q=SociologiaContenido">Contenido</a></li>
+            <li><a href="index.php?q=SociologiaMatricula">Matrícula</a></li>
+            <li><a href="index.php?q=SociologiaEvaluacion">Evaluación</a></li>
+            <li><a href="index.php?q=SociologiaCalificaciones">Calificaciones</a></li>
+        </ul>
 
-	<h2>Apuntes</h2>
-	<h3><a href="index.php?q=ApuntesMatematicas">Matematicas</a></h3>
-	<h3><a href="index.php?q=ApuntesSociologia">Sociologia</a></h3>
-	<h3><a href="index.php?q=ApuntesViejos">viejos</a></h3>
-	<h3><a href="index.php?q=Colaborar">Colaborar</a></h3>
-	<!--
-	<h3><a href="index.php?q=ApuntesCILA">Apuntes del CILA</a></h3>
-	<h3><a href="index.php?q=EjemplosCILA">Ejemplos de c&oacute;digo</a></h3>
-	<h3><a href="index.php?q=LibroCILA">Libro del CILA</a></h3>
-	-->
-
-
-	<h2>Ediciones Anteriores</h2>
-	<h3><a href="index.php?q=Noviembre2001">Noviembre 2001</a></h3>
-	<h3><a href="index.php?q=Agosto2002">Agosto 2002</a></h3>
-	<h3><a href="index.php?q=Septiembre2002">Septiembre 2002</a></h3>
-	<h3><a href="index.php?q=Octubre2002">Octubre 2002</a></h3>
-	<h3><a href="index.php?q=Septiembre2003">Septiembre 2003</a></h3>
-	<h3><a href="index.php?q=Octubre2004">Octubre 2004</a></h3>
-</div>
-<div id="drcha">
-<h1>{TITLE}</h1>
-</div>
-<div id="drcha">
+        <p>Apuntes</p>
+        
+        <ul>
+            <li><a href="index.php?q=ApuntesMatematicas">Matematicas</a></li>
+            <li><a href="index.php?q=ApuntesSociologia">Sociologia</a></li>
+            <li><a href="index.php?q=ApuntesViejos">viejos</a></li>
+            <li><a href="index.php?q=Colaborar">Colaborar</a></li>
+            <!--
+            <li><a href="index.php?q=ApuntesCILA">Apuntes del CILA</a></li>
+            <li><a href="index.php?q=EjemplosCILA">Ejemplos de c&oacute;digo</a></li>
+            <li><a href="index.php?q=LibroCILA">Libro del CILA</a></li>
+            -->
+        </ul>
+        
+        <p>Ediciones Anteriores</p>
+        <ul>
+            <li><a href="index.php?q=Noviembre2001">Noviembre 2001</a></li>
+            <li><a href="index.php?q=Agosto2002">Agosto 2002</a></li>
+            <li><a href="index.php?q=Septiembre2002">Septiembre 2002</a></li>
+            <li><a href="index.php?q=Octubre2002">Octubre 2002</a></li>
+            <li><a href="index.php?q=Septiembre2003">Septiembre 2003</a></li>
+            <li><a href="index.php?q=Octubre2004">Octubre 2004</a></li>
+        </ul> 
+        <div><a href="http://validator.w3.org/check?uri=referer"><img src="img/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a></div>
+    </div>
+    <div class="drcha">
+        <h1>{TITLE}</h1>
+        <div>
 ';
 
 $END_BODY='</div>
-	<div id="drcha">
 		<h4>Organizan:
-			<a href="http://www.fmat.ull.es"><img src="img/banner_fmat.png" alt="banner fmat" border="0"></a>
-			<a href="http://www.gulic.org/"><img src="img/banner_gulic.png" alt="banner gulic" border="0"></a>
+			<a href="http://www.fmat.ull.es"><img src="img/banner_fmat.png" alt="banner fmat" /></a>
+			<a href="http://www.gulic.org/"><img src="img/banner_gulic.png" alt="banner gulic" /></a>
 		</h4>
 	</div>
 </body>';
@@ -86,8 +100,8 @@ else
 // los {ESTOS} si metemos de {ESTOS} en la definición de <estos>
 
 $out = str_replace(
-	array('<head>', '<body>', '</body>', '{TITLE}'),
-	array($HEAD, $BEGIN_BODY, $END_BODY, $TITLE),
+	array('<html>', '<head>', '<body>', '</body>', '{TITLE}'),
+	array($HTML, $HEAD, $BEGIN_BODY, $END_BODY, $TITLE),
 	$out
 ); 
 
