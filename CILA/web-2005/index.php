b06b6db2cd690cde2729ef9b50513c70785564ca
Jonás Regueira <jregueirar@gmail.com>
2006-09-28 16:48:04 +0000

    Prueba cambio

--- CILA/web-2005/index.php
+++ CILA/web-2005/index.php
@@ -58,6 +58,7 @@ $BEGIN_BODY = '<body>
             <li><a href="index.php?q=Octubre2002">Octubre 2002</a></li>
             <li><a href="index.php?q=Septiembre2003">Septiembre 2003</a></li>
             <li><a href="index.php?q=Octubre2004">Octubre 2004</a></li>
+            <li><a href="index.php?q=Octubre2005">Octubre 2005</a></li>		
         </ul> 
         <div><a href="http://validator.w3.org/check?uri=referer"><img src="img/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a></div>
     </div>
