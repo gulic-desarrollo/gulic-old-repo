e31153cc6022fafdd7cee8234776e43afde7c03c
Jonás Regueira <jregueirar@gmail.com>
2006-11-17 22:10:46 +0000

    Ultima version

--- CILA/cila_nov06/test_automatizado/resultados.php
+++ CILA/cila_nov06/test_automatizado/resultados.php
@@ -11,17 +11,31 @@ if (!comprueba_dni($dni))
 	die("Dni incorrecto. Tu no eres alumno mio.");
 
 $ip = $_SERVER['REMOTE_ADDR'];
-if (!comprueba_examinado($ip, $dni)) 
-	die("Ya hiciste el examen pillin.");
+if (!comprueba_examinado($ip, $dni))
+  die("Ya hiciste el examen pillin. Pulsa <a href='calificaciones_ini.php'>aqui</a> para ver las notas.");
 
-dbquery("INSERT INTO `ctrl_examen` (DNI, Ip) ".
+db_query("INSERT INTO `ctrl_examen` (DNI, Ip) ".
 	"VALUES ('$dni', '$ip');");
 
-$soluciones = Array("p1" => "4", "p2" => "1", "p3" => "2");
+$soluciones = Array("p1" => "c", 
+										"p2" => "b", 
+										"p3" => "d",
+										"p4" => "c",
+										"p5" => "c",
+										"p6" => "c",
+										"p7" => "d",
+										"p8" => "c",
+										"p9" => "b",
+										"p10" =>"a",
+										"p11" =>"c",
+										"p12" =>"d",
+										"p13" =>"c",
+										"p14" =>"b",
+										"p15" =>"a",
+										"p16" =>"b",
+										"p17" =>"c",
+										);
 $preguntas = array_keys($soluciones);
-foreach($preguntas as $key) {
-	echo "$key<br>";
-}
 
 foreach ($_POST as $key => $value) {
 		if (in_array($key, $preguntas)) {
@@ -31,4 +45,9 @@ foreach ($_POST as $key => $value) {
 
 contestacion($dni, $soluciones, $respuestas);
 
+echo "<HTML>";
+echo "<BODY>";
+echo "<p>Tu examén se ha realizado con éxito. Pulsa <a href='calificaciones_ini.php'>aquí</a> para ver tu nota.</p>";
+echo "</BODY>";
+echo "</HTML>";
 ?>
