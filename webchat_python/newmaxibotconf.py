86208bf410b1941e3ed874750bff0cd38886aca4
Alberto Morales <amd77@galotecnia.com>
2007-03-05 02:28:52 +0000

    Nueva version.

---
+++ webchat_python/newmaxibotconf.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+DBURI='mysql://dbuser:dbpass@localhost/dbname'
+
+CONFIGS = [
+	{
+		'NAME': 'ejemplo1',
+		'WELCOME_MSG': 'Un texto de bienvenida de ejemplo1',
+		'JABBER_ID': 'ejemplo1@gulic.org/resource1',
+		'JABBER_PASS': 'xxxxxxxx',
+		'JABBER_ROOM': 'sala1@muc.gulic.org',
+	}
+]
+
+WELCOME_MSG = "Hola %s, soy tu bot. Puedes saber las cosas que hago en cualquier momento diciendome '\\help'. Si quieres dejar de ser mi amigo, dime '\\unsubscribe'. Si no puedes hablar con nadie, borrame de tu bandeja de contactos y vuelveme a anadir. Espero que tengamos una larga amistad."
+
+GOODBYE_MSG = "Has dejado de ser mi amigo, %s. Para completar el proceso eliminame de tu bandeja de contactos. Si quieres ser mi amigo de nuevo, anademe a tu bandeja de contactos o dime '\\subscribe'. Gracias por la amistad de todo este tiempo."
+
+
